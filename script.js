c5ff07c7988f92853942d82bcb9cc095d53ccbda
aierxx <151916792+aierxx@users.noreply.github.com>
2023-11-24 18:36:16 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('learnMoreBtn').addEventListener('click', function() {
+    // You can replace 'learn-more.html' with the actual URL of the page you want to redirect to.
+    window.location.href = 'learn-more.html';
+});
